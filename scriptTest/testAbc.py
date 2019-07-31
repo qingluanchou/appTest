0be9917729ad6788456a1cc7e532b4c21593c9b5
zengwenliang <zengwenliang@haodf.com>
2019-07-31 22:49:56 +0800

    首次提交

---
+++ scriptTest/testAbc.py
@@ -0,0 +1,33 @@
+import pytest
+import allure
+
+
+class Test_ABC:
+
+    # 函数级开始
+
+    def setup(self):
+        print("------->setup_method")
+
+    # 函数级结束
+
+    def teardown(self):
+        print("------，->teardown_method")
+
+
+    @allure.step(title='第一个测试')
+    def test_a(self):
+        allure.attach("这是一个描叙","试一下")
+
+        assert 1
+
+    @allure.issue('http://www.haodf.com/')
+    @allure.testcase('http://www.haodf.com/testabc')
+    @allure.severity('Critical')
+    def test_b(self):
+        allure.attach("这是一个描叙", "试一下")
+        print("------->test_b")
+
+
+# if __name__ == '__main__':
+#     pytest.main('-s  testAbc.py')
